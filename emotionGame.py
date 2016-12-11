61c00cbee2caa0a231394a6bb50cb3698266d395
yingding <yingdingwang@yahoo.de>
2016-12-11 14:45:49 +0100

    add async talk

--- emotionGame.py
+++ emotionGame.py
@@ -7,24 +7,53 @@ from SpeechPython.speech import Speech
 # import sys
 # import select
 # import termios
+import pygame as pg
 import subprocess
+from pygame.locals import *
 
-class RosFace(tr.Thread):
+
+endGame = False 
+
+class RosFaceTR(tr.Thread):
     def __init__(self):
-        super(RosFace, self).__init__()
+        super(RosFaceTR, self).__init__()
 
     def run(self):
         pass
         
+class AsyncTalker(tr.Thread):
+    def __init__(self):
+        super(AsyncTalker, self).__init__()
+    
+    def initSentence(self, sentence, talkId):
+        self.sentence = sentence
+        self.talkId = talkId
+  
+    def run(self):
+        global s;
+        s.create_talk(self.sentence, self.talkId)       
+        s.play_talk(self.talkId) 
 
 def main():
     # Start the game
+    global endGame
+    global s
     s = Speech()
-    s.create_talk("Welcome to the emotion game.", "id1")
-    tr1 = RosFace().start()
-    s.play_talk("id1")
+    talk = AsyncTalker()
+    talk.initSentence("Welcome to the emotion game.", "id1")
+    tr1 = RosFaceTR().start()
+    talk.run()
     
+    while not endGame:
+        for event in pg.event.get(): 
+            if event.type == QUIT:
+                endGame = True
+        pressedKey = pg.key.get_pressed()
+        #if pressedKey[K_q]
+        #    endGame = True
     
+
+pg.init()    
 main()
 
     
